0f7d76f03818e460df1a83f7e5ea4aed603e6a62
Brian-Walters-Woz-U <76453937+Brian-Walters-Woz-U@users.noreply.github.com>
2024-06-04 11:47:07 -0400

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { MessageFormComponent } from './message-form/message-form.component';
+import { GreetingComponent } from './greeting/greeting.component';
+import { SkillsComponent } from './skills/skills.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {  path: 'message',  component: MessageFormComponent },
+    {  path: 'greeting',  component: GreetingComponent },
+    {  path: 'skills',  component: SkillsComponent }
+
+];
